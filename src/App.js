0cab8de4d1856813ce0c366d07b85f048c6f6bf5
Maitetxu Darguy <maitetxu.darguy@gmail.com>
2018-10-04 11:27:32 +0200

    challenge state

--- src/App.js
+++ src/App.js
@@ -5,14 +5,29 @@ import './App.css';
 import Quotes from "./Quotes";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      on: false,
+    };
+  }
+  handleClick = () => {
+    this.setState({ on: !this.state.on, });
+  };
   render() {
+    const light = this.state.on ? 'on' : 'off';
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Simpsons Quotes</h1>
+          <div className="logoButton">
+            <img src={logo} className={light} alt="logo" />
+            <p><span>Is Homer working ? </span><button
+              className={light} onClick={this.handleClick}
+              className={light}>{light.toUpperCase()}</button></p>
+          </div>
         </header>
-        <Quotes/>
+        <Quotes />
       </div>
     );
   }
